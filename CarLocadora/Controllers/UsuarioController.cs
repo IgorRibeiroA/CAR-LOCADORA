6af2d6831243763a83bd7f7c383b40cb3c63903d
Igor Ribeiro <igo.rogerio123@gmail.com>
2022-08-07 14:22:20 -0300

    criando a viw e o controlles de Usuario, Visualizar, Adicionar e editar

---
+++ CarLocadora/Controllers/UsuarioController.cs
@@ -0,0 +1,167 @@
+﻿using CarLocadora.Modelo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace CarLocadora.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private string mensagem = "";
+
+        private readonly IOptions<DadosBase> _dadosBase;
+        public UsuarioController(IOptions<DadosBase> dadosBase)
+        {
+            _dadosBase = dadosBase;
+        }
+        // GET: UsuarioController
+        public ActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync($"{_dadosBase.Value.API_URL_BASE}Usuario").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+                return View(JsonConvert.DeserializeObject<List<Usuario>>(conteudo));
+            }
+            else
+            {
+                throw new Exception("Falha sistêmica");
+            }
+        }
+
+
+        // GET: UsuarioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] Usuario usuario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    HttpClient client = new HttpClient();
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = client.PostAsJsonAsync($"{_dadosBase.Value.API_URL_BASE}Usuario", usuario).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction(nameof(Index), new { mensagem = "resgisto incluido!", sucesso = true });
+                    }
+                    else
+                    {
+                        throw new Exception("Falha Sistêmica");
+                    }
+                }
+                else
+                {
+                    TempData["erro"] = "Algum campo pode está incorreto";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["erro"] = "algum erro aconteceu - " + ex.Message;
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Edit/5
+        public ActionResult Edit(string valor)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync($"{_dadosBase.Value.API_URL_BASE}Usuario/ObterUmUsuario?cpf={valor}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+                return View(JsonConvert.DeserializeObject<Usuario>(conteudo));
+            }
+            else
+            {
+                throw new Exception("Falha Sistêmica!");
+            }
+        }
+
+        // POST: UsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([FromForm] Usuario usuario)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    HttpClient client = new HttpClient();
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = client.PutAsJsonAsync($"{_dadosBase.Value.API_URL_BASE}Usuario", usuario).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction(nameof(Index), new { mensagem = "Registro Alterado!", sucesso = true });
+                    }
+                    else
+                    {
+                        throw new Exception("Deu Zica!");
+                    }
+                }
+                else
+                {
+                    TempData["erro"] = "Algum campo não foi preenchimento!";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["erro"] = "Algum erro aconteceu - " + ex.Message;
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
