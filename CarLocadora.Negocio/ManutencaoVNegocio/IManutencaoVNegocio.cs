5757806b371e0eefa7c698d7a1f0da37ca77728b
Igor Ribeiro <igo.rogerio123@gmail.com>
2022-08-24 15:27:01 -0300

    revis a interface e finalizei a controllerAPI de ManutencaoVeiculo

---
+++ CarLocadora.Negocio/ManutencaoVNegocio/IManutencaoVNegocio.cs
@@ -0,0 +1,20 @@
+﻿using CarLocadora.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLocadora.Negocio.ManutencaoVNegocio
+{
+    public interface IManutencaoVNegocio
+    {
+        List<ManutencaoVeiculo> ObterLista();
+        ManutencaoVeiculo ObterUmManutencaoVeiculo(int id);
+        void Incluir(ManutencaoVeiculo manutencaoVeiculo);
+        void Alterar(ManutencaoVeiculo manutencaoVeiculo);
+        void Excluir(int id);
+
+
+    }
+}
---
+++ CarLocadora.Negocio/ManutencaoVNegocio/ManutencaoVNegocio.cs
@@ -0,0 +1,51 @@
+﻿using CarLocadora.Infra;
+using CarLocadora.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLocadora.Negocio.ManutencaoVNegocio
+{
+    public class ManutencaoVNegocio : IManutencaoVNegocio
+    {
+        private readonly ControleDBContext _context;
+
+
+        public ManutencaoVNegocio(ControleDBContext context)
+        {
+            _context = context;
+        }
+
+        public void Alterar(ManutencaoVeiculo manutencaoVeiculo)
+        {
+            _context.ManutencaoVeiculos.Update(manutencaoVeiculo);
+            _context.SaveChanges();
+        }
+
+        public void Excluir(int id)
+        {
+            ManutencaoVeiculo manutencaoVeiculo = _context.ManutencaoVeiculos.Single(x => x.Id == id);
+            _context.ManutencaoVeiculos.Remove(manutencaoVeiculo);
+            _context.SaveChanges();
+        }
+
+        public void Incluir(ManutencaoVeiculo manutencaoVeiculo)
+        {
+            _context.ManutencaoVeiculos.Add(manutencaoVeiculo);
+            _context.SaveChanges();
+        }
+
+        public List<ManutencaoVeiculo> ObterLista()
+        {
+            return _context.ManutencaoVeiculos.ToList();
+        }
+
+        public ManutencaoVeiculo ObterUmManutencaoVeiculo(int id)
+        {
+            return _context.ManutencaoVeiculos.Single(x => x.Id == id);
+        }
+
+    }
+}
