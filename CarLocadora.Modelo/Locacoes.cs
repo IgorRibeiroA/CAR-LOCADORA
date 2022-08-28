7229c93a8bea8ee00a8a36377c86ceb1550859a0
Igor Ribeiro <igo.rogerio123@gmail.com>
2022-08-28 15:43:17 -0300

    Criação dos modelos Vistorias locacoes

---
+++ CarLocadora.Modelo/Locacoes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLocadora.Modelo
+{
+    public class Locacoes
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "O Id é obrigatório.")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo CPF é obrigatório.")]
+        [StringLength(14, MinimumLength = 14, ErrorMessage = "Este campo deve ter 14 caracteres")]
+        [Display(Name = "CPF do Clinte")]
+        public Cliente ClienteCPF { get; set;}
+
+        [Required(ErrorMessage = "O campo Forma Pagamento é obrigatório.")]
+        public FormaPagamento FormaPagamentoId { get; set; }
+
+        [Required(ErrorMessage = "O campo Categoria é obrigatório.")]
+        public Categoria CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "O campo Data da reserva é obrigatório.")]
+        [Display(Name = " Data da reserva")]
+        public DateTime DataHoraReserva { get; set; }
+
+        [Required(ErrorMessage = "O campo Data da retirada é obrigatório.")]
+        [Display(Name = " Data da retirada")]
+        public DateTime DataHoraRetiradaPrevista { get; set; }
+
+        [Required(ErrorMessage = "O campo Data da devolução é obrigatório.")]
+        [Display(Name = " Data da devolução")]
+        public DateTime DataHoraDevolucaoPrevista { get; set; }
+
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Este campo deve ter 8 caracteres")]
+        public Veiculo? VeiculoPlava { get; set; }
+
+        [Display(Name = "Data Inclusão")]
+        public DateTime? DataInclusao { get; set; }
+
+        [Display(Name = "Data Alteração")]
+        public DateTime? DataAlteracao { get; set; }
+
+    }
+}
---
+++ CarLocadora.Modelo/Vistoria.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLocadora.Modelo
+{
+    public class Vistoria
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "O Id é obrigatório.")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Locação é obrigatorio")]
+        public Locacoes LocacoesId { get; set; }
+
+        [Required(ErrorMessage = "O campo KM Saida é obrigatorio")]
+        [Display(Name = "KM de saida")]
+        public double KmSaida { get; set; }
+
+        [StringLength(50, ErrorMessage = "Este campo deve ter no maximo 50 caracteres")]
+        [Required(ErrorMessage = "O campo Combustivel de saida é obrigatorio")]
+        [Display(Name = "Combustivel de saida")]
+        public string CombustivelSaida { get; set; }
+
+
+        [StringLength(2000, ErrorMessage = "Este campo deve ter no maximo 2000 caracteres")]
+        [Required(ErrorMessage = "O campo Observação de Saida é obrigatorio")]
+        [Display(Name = "Observação de saida")]
+        public string? ObservacaoSaida { get; set; }
+
+
+        [Required(ErrorMessage = "O campo Observação de Saida é obrigatorio")]
+        [Display(Name = "Data de retirada")]
+        public DateTime DataHoraRetiradaPatio { get; set; }
+
+        [Required(ErrorMessage = "O campo KM Entrada é obrigatorio")]
+        [Display(Name = "KM de Entrada")]
+        public double? KmEntrada { get; set; }
+
+
+        [StringLength(50, ErrorMessage = "Este campo deve ter 50 caracteres")]
+        [Display(Name = "Combustivel de Entrada")]
+        public string? CombustivelEntrada { get; set; }
+
+
+        [StringLength(2000, ErrorMessage = "Este campo deve ter 2000 caracteres")]
+        [Display(Name = "Observação de Entrada")]
+        public string? ObservacaoEntrada { get; set; }
+
+
+        [Display(Name = "Hora da devolução")]
+        public DateTime? DataHoraDevolucaoPatio { get; set; }
+
+
+        [Display(Name = "Data Inclusão")]
+        public DateTime? DataInclusao { get; set; }
+
+        [Display(Name = "Data Alteração")]
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
